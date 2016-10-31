6c241870bb0cb00c2c8b2615f2c5142c39fc47f8
Vivek Bansal <3vivekb@gmail.com>
2016-10-31 17:25:35 +0000

    Stop ID over Stop Number

--- client/map-view/index.js
+++ client/map-view/index.js
@@ -451,7 +451,7 @@ module.exports.drawRouteStops = function (routeId, stops, isBus) {
 
                 var header = '<div class="popup-header">' + '<h5>' + stopInfo.stopName + '</h5></div>';
                 // var rtiidStr = '<strong>RTIID:</strong> ' + rtiid;
-                var rtiidStr = '<strong>Stop Number:</strong> ' + stopInfo.stopId;
+                var rtiidStr = '<strong>Stop ID:</strong> ' + stopInfo.stopId;
                 var route = '<strong>Route:</strong> ' + stopInfo.routeId + ' - ';
 
                 for (var i = 0; i < predictions.length; i++) {
