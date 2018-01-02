f788180cce5271dce37a7bda9ebe5b7eb8992fa5
bijanmmarkes <bijan@stagerap.com>
2018-01-02 12:48:39 -0800

    Fixed puppeteer viewport to go based off document body height / width.

--- puppeteer.js
+++ puppeteer.js
@@ -1,22 +1,22 @@
 const puppeteer = require("puppeteer");
 const url = process.argv[2];
-
 function createRandomString( length )
 {
-    var str = "";
-    for ( ; str.length < length; str += Math.random().toString( 36 ).substr( 2 ) );
+    for ( var str = ""; str.length < length; str += Math.random().toString( 36 ).substr( 2 ) );
     return str.substr( 0, length );
 }
 
 var img = createRandomString(16);
-
+const path = "snapshots/" + img + '.png'
 puppeteer.launch().then(async browser => {
     const page = await browser.newPage();
-    await page.setViewport({"width":1200, "height":1000});
+    await page.setViewport({"width":await page.evaluate(() => document.body.clientWidth), "height":await page.evaluate(() => document.body.clientHeight)});
+    //await page.setViewport({"width":1200, "height":1200});
     await page.goto(url, {waitUntil: 'networkidle2'});
     await page.reload();
-    await page.waitFor(7000);
-    await page.screenshot({path: img + '.png', fullPage: true});
+    await page.waitFor(7500);
+    await page.screenshot({path: path, fullPage: true});
     await browser.close();
-    console.log(img + '.png');
+    console.log(path);
 });
+
